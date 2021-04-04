8d9eb1530d7df70f86e44efd3e8edc4aa2929fd8
renatolins <renatolinsren@gmail.com>
2021-04-04 01:51:33 -0300

    fix: footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ import styles from './styles.module.css'
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <div className={styles.FooterText}><HiCode size={20} /> com <FaHeart /> por <a href="#">Renato Lins</a> utilizando <a href="#"><FaReact size={20}/></a> </div>
+      <div className={styles.FooterText}><HiCode size={20} /> com <FaHeart /> por <a href="https://renatolins.dev.br" target="_blank">Renato Lins</a> utilizando <a href="https://reactjs.org/" target="_blank"><FaReact size={20}/></a> </div>
     </footer>
   )
 }
